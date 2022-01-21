3f60299f6f1b3869a9ea95a1b7145a72dd1c7aa3
tomaszpankowski <pankowski.email@gmail.com>
2022-01-21 13:07:15 +0100

    modified routing

--- src/config.js
+++ src/config.js
@@ -1,16 +1,22 @@
 import {Redirect} from "react-router-dom";
+import Ballet from "./views/ballet";
 import Comingsoon from "./views/comingsoon";
 import Contact from "./views/contact";
 import Error from "./views/error";
+import History from "./views/history";
+import Home from "./views/home";
 import Login from "./views/login";
 import PrivacyPolicy from "./views/privacy";
 import Register from "./views/register";
 
 const appCfg = {
     routes:[
-        {navItem:true, exact:true, path:"/",name:"Comingsoon",view:<Comingsoon/>},
+        {navItem:true, exact:true, path:"/",name:"Home",view:<Home/>},
+        {navItem:true, exact:true, path:"/history",name:"History",view:<History/>},
+        {navItem:true, exact:true, path:"/ballet",name:"Ballet",view:<Ballet/>},
         {navItem:true, exact:true, path:"/contact",name:"Contact",view:<Contact/>},
         {navItem:true, exact:true, path:"/login",name:<span className="fa fa-user-circle"></span>,view:<Login backLink={{name:" Register now!",href:"/register"}}/>},
+        {navItem:false, exact:true, path:"/comingsoon",name:"Comingsoon",view:<Comingsoon/>},
         {navItem:false, exact:true, path:"/register",name:"Register",view:<Register backLink={{name:" Login now!",href:"/login"}}/>},
         {navItem:false, exact:false, path:"/error",name:"Error",view:<Error backLink={{name:"back to Home",href:"/"}}/>},
         {navItem:false, exact:true, path:"/privacy",name:"Privacy",view: <PrivacyPolicy backLink={{name:"back to Home",href:"/"}}/>},
---
+++ src/views/history.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class History extends Component{
+    render(){
+        return(        
+            <Container fluid className="history-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default History;
